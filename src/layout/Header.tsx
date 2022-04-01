0c2aa5d0146e4b06de0ebc3bd0385ae4a0f8f697
Alba Prats Tena <albaprate@MacBook-Pro-de-Alba.local>
2022-04-01 21:07:41 +0200

    contenido header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="flex justify-between border-black bg-orange-100 p-3 uppercase">
+      <div className="flex items-center">
+        <ul className="flex flex-wrap text-sm">
+          <li className="mr-6">
+            <button type="button">Menu</button>
+          </li>
+          <li className="mr-6">
+            <Link href="/">
+              <a>logo</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex items-center">
+        <ul className="flex flex-wrap text-sm">
+          <li className="mr-6">
+            <Link href="/products/">
+              <a className="border-none text-gray-700 hover:text-gray-900">
+                Galeria
+              </a>
+            </Link>
+          </li>
+          <li className="mr-6">
+            <Link href="/sesionInit/">
+              <a className="border-none text-gray-700 hover:text-gray-900">
+                iniciar sesión
+              </a>
+            </Link>
+          </li>
+
+          <li className="mr-6">cesta</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
